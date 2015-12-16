73465fda3e84e7c2fa281d9b7d1b1a8111998d20
hzlshbz <hzlshbz@users.noreply.github.com>
2015-12-16 08:22:04 +0200

    Update Pipe.java

--- src/Game_State/Pipe.java
+++ src/Game_State/Pipe.java
@@ -1,24 +1,45 @@
 package Game_State;
 
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.io.InputStream;
+
+import javax.imageio.ImageIO;
+
 public class Pipe extends PhysicalObject {
 	int speedX;
-	int speedY;
-	int x;
-	
-	public Pipe() {
-		// TODO Auto-generated constructor stub
-	}
+	double x ;
+	double y ;
+	String name;
+	BufferedImage pipeImage;
+
+	public Pipe(String name) {
+		this.name = name;	
+		}
 	public int getSpeedX() {
 		return speedX;
 	}
 	public void setSpeedX(int speedX) {
 		this.speedX = speedX;
 	}
-	public int getSpeedY() {
-		return speedY;
+	public String getName() {
+		return name;
 	}
-	public void setSpeedY(int speedY) {
-		this.speedY = speedY;
+	public double getLocation(){
+		return x;
 	}
-	
+	public void setLocation(double x){
+		this.x = x;
+	}
+	public BufferedImage getImage(String name){
+		InputStream input = this.getClass().getClassLoader().getResourceAsStream(name);
+		if (input == null) System.out.println("input null");
+		try {
+			pipeImage = ImageIO.read(input);
+			return pipeImage;
+		} catch (IOException e) {
+			e.printStackTrace();
+			return null;
+		}
+	}
 }
