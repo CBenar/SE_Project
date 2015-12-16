b0124d854e0756c639680a71c729224c5925471a
hzlshbz <hzlshbz@users.noreply.github.com>
2015-12-16 12:13:50 +0200

    Update Bird.java

--- src/Game_State/Bird.java
+++ src/Game_State/Bird.java
@@ -1,38 +1,57 @@
-package Game_GUI;
+package Game_State;
 
-import java.awt.Dimension;
-import java.awt.GridBagLayout;
-import java.awt.event.KeyEvent;
-import java.awt.event.KeyListener;
+import java.awt.Image;
 import java.awt.image.BufferedImage;
 import java.io.IOException;
 import java.io.InputStream;
 
 import javax.imageio.ImageIO;
-import javax.swing.Icon;
-import javax.swing.ImageIcon;
-import javax.swing.JButton;
-import javax.swing.JPanel;
 
-import Game_State.Bird;
+public class Bird extends PhysicalObject {
+	String name;
+	int speedY;
+	int life;
+	double x, y;
+	BufferedImage birdImage;
+	public Bird(String name) {
+		this.name=name	;
+	}
 
-public class BirdPanel extends JPanel {
-	
-	
-	public BirdPanel() {
-		// TODO Auto-generated constructor stub	
-		this.setLayout(new GridBagLayout());
-		
-		
-		this.setMaximumSize(new Dimension(600,600));
-		this.setVisible(true);
+	public String getName() {
+		return name;
 	}
-	
-	public JButton createBirdButton(Bird bird , String name){
-		JButton selectionBird = new JButton(new ImageIcon(bird.getImage(name)));
-		selectionBird.setPreferredSize(new Dimension(200,200));
-		return selectionBird;
-		
+	public void setName(String name) {
+		this.name = name;
 	}
-	
+	public int getSpeedY() {
+		return speedY;
+	}
+	public void setSpeedY(int speedY) {
+		this.speedY = speedY;
+	}
+	public void setLocation(double x, double y) {
+		this.x = x;
+		this.y = y;
+	}
+	public int getLife() {
+		return life;
+	}
+	public void setLife(int life) {
+		this.life = life;
+	}
+	public double getLocation(){
+		return x;
+
+	}
+	public BufferedImage getImage(String name){
+		InputStream input = this.getClass().getClassLoader().getResourceAsStream(name);
+		if (input == null) System.out.println("input null");
+		try {
+			birdImage = ImageIO.read(input);
+			return birdImage;
+		} catch (IOException e) {
+			e.printStackTrace();
+			return null;
+		}
+	}
 }
