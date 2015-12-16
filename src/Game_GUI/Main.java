65ff0d507d57dda81d592179772d212a2ceaaba3
hzlshbz <hzlshbz@users.noreply.github.com>
2015-12-16 12:12:32 +0200

    Update Main.java

--- src/Game_GUI/Main.java
+++ src/Game_GUI/Main.java
@@ -1,6 +1,8 @@
 package Game_GUI;
 
 import java.awt.Color;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
@@ -11,7 +13,7 @@ public class Main {
 	
 	public static void main(String[] args) {
 		JFrame mainFrame = new JFrame();
-		mainFrame.setSize(800, 650);
+		mainFrame.setSize(800, 600);
 		
 		
 		
@@ -35,8 +37,21 @@ public class Main {
 		birdP.setVisible(false);
 		System.out.println(controller.flag);
 	
+		mainFrame.addKeyListener(new KeyListener(){ 
+
+		    public void keyPressed(KeyEvent ke){ 
+
+		         if(ke.getKeyCode()==KeyEvent.VK_SPACE){
+		              System.out.println("yes");
+		         }
+		    }
+		
+			public void keyReleased(KeyEvent arg0) {}
+			public void keyTyped(KeyEvent arg0) {}
+		});
 
 
 		
 	}
+	
 }
