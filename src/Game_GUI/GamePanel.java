df019c8536593d2ed585dcd3d6f43bd7ac462448
hzlshbz <hzlshbz@users.noreply.github.com>
2015-12-16 12:12:10 +0200

    Update GamePanel.java

--- src/Game_GUI/GamePanel.java
+++ src/Game_GUI/GamePanel.java
@@ -1,15 +1,18 @@
-
 package Game_GUI;
 
 import java.awt.Dimension;
 import java.awt.Graphics;
 import java.awt.Image;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 import java.awt.image.BufferedImage;
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 
 import javax.imageio.ImageIO;
 import javax.swing.Icon;
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
@@ -21,101 +24,44 @@ public class GamePanel extends JPanel {
 	Bird selectedBird;
 	BufferedImage birdImage;
 	BufferedImage background;
+	public JLabel backgroundLabel;
+	public JLabel birdLabel;
 	int x = 10;
 
 	public Pipe pipeUp = new Pipe("pipe_north.png");
 	public Pipe pipeDown = new Pipe("pipe_south.png");
 
 	JButton start;
-	//Icon rune;
+
 	public GamePanel(Bird bird ) {
 		this.selectedBird = bird;
-
+		birdLabel = new JLabel ( new ImageIcon(bird.getImage(selectedBird.getName())));
+		backgroundLabel = new JLabel ( new ImageIcon(getImage("x.jpg")));
+		birdLabel.setBounds(400,300,50,50);
+		backgroundLabel.setBounds(0, 0, 800, 600);
 		this.setMaximumSize(new Dimension(500,400));
-
+		
+		this.setLayout(null);
+		this.add(backgroundLabel);
+		backgroundLabel.add(birdLabel);
 		this.setVisible(true);
+
 	}
-
-	public Image createBackground() throws IOException{
-
-		InputStream input = this.getClass().getClassLoader().getResourceAsStream("newBG.png");
-
-		background = ImageIO.read(input);
-
-		return background;		
-	}
-
-	public void paint(Graphics g) {
+	public BufferedImage getImage(String name){
+		InputStream input = this.getClass().getClassLoader().getResourceAsStream(name);
+		if (input == null) System.out.println("input null");
 		try {
-			g.drawImage(createBackground(), 0, 0 , null);
+			birdImage = ImageIO.read(input);
+			return birdImage;
 		} catch (IOException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
+			return null;
 		}
-		g.drawImage(selectedBird.getImage(selectedBird.getName()), 100, 300 ,null);
-
-		g.drawImage(pipeUp.getImage(pipeUp.getName()),
-				(int) pipeUp.getLocation() - 60, 400, null);
-		System.out.println(pipeUp.getLocation() );
-		pipeUp.setLocation(pipeUp.getLocation() - 35);
-		// pipeUp.setLocation(600+x);
-		// System.out.println(pipeUp.getLocation()+"sdafadsad");
-		if (pipeUp.getLocation() < 0) {
-			pipeUp.setLocation(600);
-			g.drawImage(pipeUp.getImage(pipeUp.getName()),
-					(int) pipeUp.getLocation(), 400, null);
-			this.repaint();
-
-		}
-		this.repaint();
-		
-		g.drawImage(pipeDown.getImage(pipeDown.getName()),
-				(int) pipeDown.getLocation() - 60, -150, null);
-		System.out.println(pipeDown.getLocation() );
-		pipeDown.setLocation(pipeDown.getLocation() - 35);
-		// pipeUp.setLocation(600+x);
-		// System.out.println(pipeUp.getLocation()+"sdafadsad");
-		if (pipeDown.getLocation() < 0) {
-			pipeDown.setLocation(600);
-			g.drawImage(pipeDown.getImage(pipeDown.getName()),
-					(int) pipeDown.getLocation(), -150, null);
-			this.repaint();
-
-		}
-		this.repaint();
-		
-		
-		g.drawImage(pipeDown.getImage(pipeDown.getName()),
-				(int) pipeDown.getLocation() - 350, -120, null);
-		System.out.println(pipeDown.getLocation() );
-		pipeDown.setLocation(pipeDown.getLocation() - 35);
-		// pipeUp.setLocation(600+x);
-		// System.out.println(pipeUp.getLocation()+"sdafadsad");
-		if (pipeDown.getLocation() < 0) {
-			pipeDown.setLocation(600);
-			g.drawImage(pipeDown.getImage(pipeDown.getName()),
-					(int) pipeDown.getLocation(), -120, null);
-			this.repaint();
-
-		}
-		this.repaint();
-		
-
-		g.drawImage(pipeUp.getImage(pipeUp.getName()),
-				(int) pipeUp.getLocation() - 400, 450, null);
-		System.out.println(pipeUp.getLocation() );
-		pipeUp.setLocation(pipeUp.getLocation() - 35);
-		// pipeUp.setLocation(600+x);
-		// System.out.println(pipeUp.getLocation()+"sdafadsad");
-		if (pipeUp.getLocation() < 0) {
-			pipeUp.setLocation(600);
-			g.drawImage(pipeUp.getImage(pipeUp.getName()),
-					(int) pipeUp.getLocation(), 120, null);
-			this.repaint();
-
-		}
-		this.repaint();
-
-
 	}
+	
 }
+
+
+
+
+
